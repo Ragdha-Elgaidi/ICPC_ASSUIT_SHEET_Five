1208e25cf0692f9367be89f8d694f6750efbd17e
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-10-30 06:38:53 +0200

    Update C. Wonderful Number.cpp

--- C. Wonderful Number.cpp
+++ C. Wonderful Number.cpp
@@ -1,1 +1,40 @@
 
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int num;
+	cin >> num; 
+
+	if (num % 2 == 0)
+	{  
+		cout << "NO" << endl;
+		return 0;
+	}
+
+	int index = 0;
+	int bi[100];
+
+
+	while (num != 0)
+	{
+		bi[index] = num % 2;
+		num /= 2; 
+		index++; 
+	}
+
+	for (int i = 0, z = index - 1; i < index, z >= 0; i++, z--)
+    {
+		if (bi[i] != bi[z]) 
+    {
+			cout << "NO" << endl;
+			return 0;
+		}
+	}
+
+	cout << "YES" << endl;
+}
