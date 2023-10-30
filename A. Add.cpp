38ee1ea4f26ddf826cc14b81e8c93fa79d9b7abc
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-10-30 05:24:13 +0200

    Update A. Add.cpp

--- A. Add.cpp
+++ A. Add.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int sum(int x, int y)
+{
+	int z = x + y;
+	return z;
+}
+int main()
+{
+	int x, y;
+	cin >> x >> y;
+	cout<<sum(x, y);
+}
 
