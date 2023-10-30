b8ce167a190af888e4b1a71a9ec6a54d10a01021
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-10-30 05:30:47 +0200

    Update B. Print.cpp

--- B. Print.cpp
+++ B. Print.cpp
@@ -1,1 +1,25 @@
 
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+void print(int n)
+{
+	for (int i = 1; i <= n; i++)
+	{
+		cout << i;
+		if(i!=n)
+		{
+			cout << " ";
+		}
+	}
+
+}
+int main()
+{
+	int n;
+	cin >> n;
+	print(n);
+}
