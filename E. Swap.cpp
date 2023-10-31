cfd5d30fcf08f2aab6b5484e68ac57e12f58fe06
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-10-31 20:53:42 +0200

    Update E. Swap.cpp

--- E. Swap.cpp
+++ E. Swap.cpp
@@ -1,1 +1,27 @@
 
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+void Swap(int& n1, int& n2)
+{
+	int temp;
+	temp = n1;
+	n1 = n2;
+	n2 = temp;
+}
+
+int main()
+{
+	int n1, n2;
+
+	cin >> n1 >> n2;
+
+	Swap(n1, n2);
+
+	cout << n1 << " " << n2;
+
+}
