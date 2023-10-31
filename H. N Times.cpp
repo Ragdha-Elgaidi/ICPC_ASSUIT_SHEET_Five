eefd161532c68b71017fc528236cae9b5f594fad
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-10-31 22:05:43 +0200

    Update H. N Times.cpp

--- H. N Times.cpp
+++ H. N Times.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
 
+using namespace std;
+
+void NTimes(int n, char c)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << c <<" ";
+	}
+}
+
+int main()
+{
+	int t, n; char c;
+
+	cin >> t;
+
+	while (t--)
+	{
+		cin >> n >> c;
+
+		NTimes(n, c);
+		cout << endl;
+	}
+	
+	return 0;
+}
