792495fe1def6fb51e650018d9e147817e3888b7
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-10-31 20:41:28 +0200

    Update D. Prime Function.cpp

--- D. Prime Function.cpp
+++ D. Prime Function.cpp
@@ -1,1 +1,51 @@
 
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+
+bool Prime(int n)
+{
+	int m =sqrt(n);
+
+	for (int i = 2; i <= m; i++)
+	{
+		if ((n % i) == 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+int main()
+{
+	int t, n;
+
+	cin >> t;
+
+	while (t--)
+	{
+		cin >> n;
+
+		if (n == 1) 
+		{
+			cout << "NO" << endl;
+			continue;
+		}
+
+		bool prime = Prime(n);
+
+		if (prime == true)
+		{
+			cout << "YES" << endl;
+		}
+		else
+		{
+			cout << "NO" << endl;
+		}
+	}
+}
