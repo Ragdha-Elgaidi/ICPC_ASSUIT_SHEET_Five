cd3c2137b682365b40227b137b80934500bd7311
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-10-31 23:11:59 +0200

    Update J. Average .cpp

--- J. Average .cpp
+++ J. Average .cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
 
+using namespace std;
+
+double Sum(int n)
+{
+	double sum = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		double value;
+
+		cin >> value;
+
+		sum += value;
+	}
+
+	return sum;
+}
+
+int main()
+{
+	int n;
+
+	cin >> n;
+	cout << setprecision(7) << Sum(n) / n << endl;
+
+	return 0;
+}
