5ddefdb3ca26f0f1314f0df9ca79d2e7277d6691
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-10-31 21:55:44 +0200

    Update G. Max and MIN.cpp

--- G. Max and MIN.cpp
+++ G. Max and MIN.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
 
+using namespace std;
+void MinMax(int arr[], int n)
+{
+	sort(arr, arr + n);
+
+	cout<<arr[0]<<" "<< arr[n - 1];
+}
+
+int main()
+{
+	int n, arr[1000];
+
+	cin >> n;
+
+	
+	
+		for (int i = 0; i < n; i++)
+		{
+			cin >> arr[i];
+		}
+	
+	 MinMax(arr, n);
+}
