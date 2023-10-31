717c810ef8a45a8dad2c318ea1dba6aa8c2f28f7
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-10-31 21:11:34 +0200

    Update F. Equation.cpp

--- F. Equation.cpp
+++ F. Equation.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
 
+using namespace std;
+
+long long SumOfPower(int n, int p)
+{
+	long long s = 0;
+
+	for (int i = 2; i <= p; i += 2)
+	{
+			s += pow(n, i);
+	}
+
+	return s;
+}
+
+int main()
+{
+	int n, p;
+
+	cin >> n >> p;
+
+	cout << SumOfPower(n, p) << endl;
+
+}
