6549b47ef32a3ca955c3f1825e9215038fe62330
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-11-01 05:43:09 +0200

    Update M. Distinct Numbers.cpp

--- M. Distinct Numbers.cpp
+++ M. Distinct Numbers.cpp
@@ -1,1 +1,45 @@
 
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+void Distinct(int arr[1000], int n)
+{
+	
+	sort(arr, arr + n);
+
+	int counter = 1;
+
+	for (int i = 1; i < n; i++)
+	{
+		if (arr[i] != arr[i - 1])
+		{
+			counter++;
+		}
+	}
+
+	cout << counter;
+}
+
+int main()
+{
+	int n, arr[1000];
+
+	cin >> n;
+
+	if (n == 0)
+	{
+		cout << 0;
+		return 0;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+
+	Distinct(arr, n);
+}
