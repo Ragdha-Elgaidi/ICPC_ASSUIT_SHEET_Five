1c5187bfeca0d101d4851ca4e9729cc2cf759412
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-11-01 05:12:08 +0200

    Update K. Shift Right .cpp

--- K. Shift Right .cpp
+++ K. Shift Right .cpp
@@ -1,1 +1,36 @@
 
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+const int s = 10001;
+
+int main()
+{
+	int n, x, arr[s];
+
+	cin >> n >> x;
+
+	int sh = x % n;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+
+	for (int i = n - sh; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+	for (int i = 0; i < n - sh; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+	return 0;
+}
