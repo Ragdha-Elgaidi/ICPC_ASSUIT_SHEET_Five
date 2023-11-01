94cf4de7ce816d95f3d47ea9371abd5c0360cc7d
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-11-01 05:58:29 +0200

    Update N. Shift Zeros.cpp

--- N. Shift Zeros.cpp
+++ N. Shift Zeros.cpp
@@ -1,1 +1,43 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
 
+using namespace std;
+
+void ShiftingZeros(int arr[1000], int n)
+{
+	int counter = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] == 0)
+		{
+			counter++;
+		}
+		else
+		{
+			cout << arr[i] << " ";
+		}
+	}
+	for (int i = 0; i < counter; i++) 
+	{
+		cout << 0<<" ";
+	}
+}
+
+
+int main()
+{
+	int n, arr[1000];
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+
+	ShiftingZeros(arr, n);
+}
