6fc5f40353814c3d76b91922c9100c811145a58e
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-11-01 05:16:35 +0200

    Update L. New Array.cpp

--- L. New Array.cpp
+++ L. New Array.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
 
+using namespace std;
+
+const int s = 10001;
+
+void print(int arr[s], int size) 
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+}
+int main()
+{
+	int size;
+
+	cin >> size;
+
+	int arr1[s];
+	int arr2[s];
+
+	for (int i = 0; i < size; i++)
+	{
+		cin >> arr1[i];
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		cin >> arr2[i];
+	}
+
+	print(arr2, size);
+	print(arr1, size);
+
+	return 0;
+}
